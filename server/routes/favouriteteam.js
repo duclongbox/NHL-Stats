1808f7301d747d6969d48a78ef0a5daf00d90c22
DLong <duclongbox@gmail.com>
2024-08-09 23:18:10 -0400

    favourite endpoint

--- server/routes/favouriteteam.js
+++ server/routes/favouriteteam.js
@@ -27,4 +27,19 @@ router.get('/', async (req, res) => {
     }
 }); 
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const teamId = req.params.id;
+        
+        const deletedTeam = await FavouriteTeams.findOneAndDelete({ id: teamId });
+        
+        if (!deletedTeam) {
+            return res.status(404).json({ message: 'Team not found' });
+        }
+        res.status(200).json({ message: 'Team deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete favourite team' });
+    }
+});
+
 module.exports = router;
